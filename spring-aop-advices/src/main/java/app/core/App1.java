a6b4eb24f197fa6a2333003ebbdd00a9274cb0e4
jbt <jbt@404_INS>
2022-07-12 13:56:08 +0300

    AAA

--- spring-aop-advices/src/main/java/app/core/App1.java
+++ spring-aop-advices/src/main/java/app/core/App1.java
@@ -2,6 +2,8 @@ package app.core;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import app.core.beans.Calculator;
+
 public class App1 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,15 @@ public class App1 {
 		// create a spring container here
 		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);) {
 
+			Calculator calculator = ctx.getBean(Calculator.class);
+			int a = 25;
+			int b = 0;
+
+			String res = calculator.divide(a, b);
+
+			System.out.println(res);
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 
 	}
